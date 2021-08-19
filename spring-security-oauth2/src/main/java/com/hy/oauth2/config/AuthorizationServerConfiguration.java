543026667b328943fd50620bad6886b802d5bc04
wyl <791128058@qq.com>
2021-08-19 13:27:42 +0800

    基于jdbc存储客户端

--- spring-security-oauth2/src/main/java/com/hy/oauth2/config/AuthorizationServerConfiguration.java
+++ spring-security-oauth2/src/main/java/com/hy/oauth2/config/AuthorizationServerConfiguration.java
@@ -1,10 +1,23 @@
 package com.hy.oauth2.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * 认证服务配置
@@ -12,23 +25,57 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    // 注入 WebSecurityConfiguration 中配置的 BCryptPasswordEncoder
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        // 配置数据源（注意，我使用的是 HikariCP 连接池），以上注解是指定数据源，否则会有冲突
+        return DataSourceBuilder.create().build();
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         // 配置客户端
-        clients
+        /*clients
                 // 使用内存设置
                 .inMemory()
                 // client_id
                 .withClient("client")
-                // client_secret
-                .secret("secret")
+                // client_secret  // 还需要为 secret 加密
+                .secret(passwordEncoder.encode("secret"))
                 // 授权类型
                 .authorizedGrantTypes("authorization_code")
                 // 授权范围
                 .scopes("app")
                 // 自动同意授权
-                .autoApprove("true")
+                //.autoApprove("true")
                 // 注册回调地址
-                .redirectUris("http://10.20.31.252:8800/#/trafficOrder/home/index");
+                .redirectUris("http://www.funtl.com");*/
+        // 读取客户端配置
+        clients.withClientDetails(jdbcClientDetails());
     }
+
+    @Bean
+    public TokenStore tokenStore() {
+        // 基于 JDBC 实现，令牌保存到数据
+        return new JdbcTokenStore(dataSource());
+    }
+
+    @Bean
+    public ClientDetailsService jdbcClientDetails() {
+        // 基于 JDBC 实现，需要事先在数据库配置客户端信息
+        return new JdbcClientDetailsService(dataSource());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // 设置令牌
+        endpoints.tokenStore(tokenStore());
+    }
+
 }
