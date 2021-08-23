92989efa4077b4c7e3fa7781792aed79dbd15dcf
wyl <791128058@qq.com>
2021-08-24 06:43:56 +0800

    前后端分离配置

--- security-demo/security-demo1/src/main/java/com/hy/security/demo/config/WebSecurityConfig.java
+++ security-demo/security-demo1/src/main/java/com/hy/security/demo/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.hy.security.demo.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsUtils;
+
+import java.io.PrintWriter;
 
 /**
  * @author wyl
@@ -16,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        /*http
                 // 不配置/login post 403
                 .csrf()
                 .disable()
@@ -34,9 +37,63 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login")
                 .permitAll(true)
                 .and()
-                .httpBasic()
-                .and();
+                .httpBasic();*/
 
+        http
+                // 不配置/login post 403
+                .csrf()
+                .disable()
+                // 跨域
+                .cors()
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest)
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .successHandler((req, resp, authentication) -> {
+                    Object principal = authentication.getPrincipal();
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(principal));
+                    out.flush();
+                    out.close();
+                })
+                .failureHandler((req, resp, e) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(e.getMessage());
+                    out.flush();
+                    out.close();
+                })
+                .loginPage("http://localhost:9081/login.html")
+                .loginProcessingUrl("/login")
+                .permitAll(true)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint((req, resp, authException) -> {
+                            resp.setContentType("application/json;charset=utf-8");
+                            PrintWriter out = resp.getWriter();
+                            out.write("尚未登录，请先登录");
+                            out.flush();
+                            out.close();
+                        }
+                )
+                .and()
+                .httpBasic()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessHandler((req, resp, authentication) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write("注销成功");
+                    out.flush();
+                    out.close();
+                })
+                .permitAll();
     }
 
 }
